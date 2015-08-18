337a61b9e952a6d7de880ebf9253fd85720d4f4b
Brayden Cleary <brayden.cleary@gmail.com>
2015-08-17 23:19:58 -0700

    working

--- controller.py
+++ controller.py
@@ -7,144 +7,70 @@ from wtforms.validators import Required, Optional
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_iris
+from datetime import datetime
 import pickle
-
-
 
 #Initialize Flask App
 app = Flask(__name__)
 
-#Initialize Form Class
-# This form will take in the form data on the front end and use it to predict
-# using a pre-loaded model in the model folder!!!!
-class PredictForm(Form):
-	sepal_length = DecimalField('Sepal Length (cm):', places=2, validators=[Required()])
-	sepal_width = DecimalField('Sepal Width (cm):', places=2, validators=[Required()])
-	petal_length = DecimalField('Petal Length (cm):', places=2, validators=[Required()])
-	petal_width = DecimalField('Petal Width (cm):', places=2, validators=[Required()])
-	submit = SubmitField('Submit')
+my_model = pickle.load(open('my_model.pkl'))
 
-
-# this will take in the form data on the front end and train the model and store it in the model folder!
-class TrainForm(Form):
-	n_neighb = SelectField('Number of Neighbors:', choices=[(2,2),(3,3),(4,4),(5,5),(6,6)],coerce=int)
-	param_2 = DecimalField('Parameter 2 (Optional)', places = 2, validators=[Optional()])
-	param_3 = DecimalField('Parameter 3 (Optional)', places = 2, validators=[Optional()])
-	param_4 = DecimalField('Parameter 4 (Optional)', places = 2, validators=[Optional()])
-	submit = SubmitField('Submit')
-
-
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def model():
-	
-	prediction, sepal_length, sepal_width, petal_length, petal_width, n_neighb  = None, None, None, None, None, None
-	train_form = TrainForm(csrf_enabled=False)
-	predict_form = PredictForm(csrf_enabled=False)
+  SAN_FRANCISCO_ZIP_CODES = [94102, 94109, 94123, 94117, 94134, 94112, 94124, 94121, 94133, 94116, 94115, 94110, 94127, 94114, 94107, 94132, 94122, 94103, 94105, 94104, 94108, 94118, 94158, 94111, 94131, 94130, 94014, 94129, 94015]
+
+  WEEKEND_DAYS = ['Saturday', 'Sunday']
+
+  EARLY_MORNING   = [5,6,7]
+  LATE_MORNING    = [8,9,10]
+  EARLY_AFTERNOON = [11,12,13]
+  LATE_AFTERNOON  = [14,15,16]
+  EARLY_EVENING   = [17,18,19]
+  LATE_EVENING    = [20,21,22]
+  EARLY_NIGHT     = [23,0,1]
+  LATE_NIGHT      = [2,3,4]
+
+  def determine_time_of_day_bucket(datetime):
+    hour = datetime.hour
+    if hour in EARLY_MORNING:
+        return 1
+    elif hour in LATE_MORNING:
+        return 2
+    elif hour in EARLY_AFTERNOON:
+        return 3
+    elif hour in LATE_AFTERNOON:
+        return 4
+    elif hour in EARLY_EVENING:
+        return 5
+    elif hour in LATE_EVENING:
+        return 6
+    elif hour in EARLY_NIGHT:
+        return 7
+    elif hour in LATE_NIGHT:
+        return 8
 
 
+  def parse_for_map(datetime):
+    zip_predictions = []
+    for zip in SAN_FRANCISCO_ZIP_CODES:
+      day_of_month        = datetime.day
+      time_of_day_bucket  = determine_time_of_day_bucket(datetime)
+      month_of_year       = datetime.month
+      year                = datetime.year
+      DayOfWeek_Friday    = datetime.isoweekday() == 5
+      DayOfWeek_Wednesday = datetime.isoweekday() == 3
+      DayOfWeek_Tuesday   = datetime.isoweekday() == 2
+      DayOfWeek_Thursday  = datetime.isoweekday() == 4
+      DayOfWeek_Monday    = datetime.isoweekday() == 1
+      DayOfWeek_Saturday  = datetime.isoweekday() == 6
+      DayOfWeek_Sunday    = datetime.isoweekday() == 7
+      is_weekend          = datetime.isoweekday() in [6,7]
+      zip_predictions.append({'zip': zip, 'result': my_model.predict([day_of_month, time_of_day_bucket, month_of_year, year, DayOfWeek_Friday, DayOfWeek_Wednesday, DayOfWeek_Tuesday, DayOfWeek_Thursday, DayOfWeek_Monday, DayOfWeek_Saturday, DayOfWeek_Sunday, is_weekend, zip])[0]})
+    return zip_predictions
 
-													######################
-													# Training The Model #
-													######################
+  zip_preds = parse_for_map(datetime.now())
 
-	if train_form.validate_on_submit():
-
-		# store the submitted values
-		submitted_data = train_form.data
-		print submitted_data
-
-
-		#Retrieve values from form
-		n_neighb = submitted_data['n_neighb']
-		param_2 = submitted_data['param_2']
-		param_3 = submitted_data['param_3']
-		param_4 = submitted_data['param_4']
-		# notice I'm using the same names I used in my TrainForm class up there!!!! On line 29
-
-
-														######
-														# ML #
-														######
-		#Load Iris Data
-		iris_data = load_iris()
-		features = iris_data.data
-		feature_names = iris_data.feature_names
-		target = iris_data.target
-		target_names = iris_data.target_names
-
-
-		knn = KNeighborsClassifier(n_neighbors = n_neighb)    # replace with your own ML model here!!!!!
-		knn.fit(features, target)
-
-													############
-													# Pickling #
-													############
-		with open('model/my_model.pkl', 'wb') as f:           
-			pickle.dump(knn, f)
-
-
-
-
-		print "Trained"										  # output to tell us we are done
-
-
-
-
-
-
-													##############
-													# Predicting #
-													##############
-
-	elif predict_form.validate_on_submit():
-
-		# store the submitted values
-		submitted_data = predict_form.data
-		print submitted_data
-
-		#Retrieve values from form
-		sepal_length = submitted_data['sepal_length']
-		sepal_width = submitted_data['sepal_width']
-		petal_length = submitted_data['petal_length']
-		petal_width = submitted_data['petal_width']
-		# notice I'm using the same names I used in my PredictForm class up there!!!! On line 20
-
-		#Create array from values
-		flower_instance = [sepal_length, sepal_width, petal_length, petal_width]
-
-		# unpickle my model
-		knn = pickle.load(open('model/my_model.pkl'))
-
-		# the machine learning part!!!
-		my_prediction = knn.predict(flower_instance)
-
-
-		# need to get the target_names again!
-		iris_data = load_iris()
-		target_names = iris_data.target_names
-		# so our output is pretty
-
-		# Return only the Predicted iris species
-		prediction = target_names[my_prediction][0].capitalize()
-		# This variable ends up getting put in bold on the front end!
-		# When you insert your own ML model, make sure your output is called "prediction"
-
-
-	return render_template(
-		'model.html',
-		predict_form=predict_form, 
-		train_form = train_form, 
-		prediction=prediction)
-
-
-
-
-
-
-#Handle Bad Requests
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'), 404
+  return render_template('model.html', data=zip_preds)
 
 if __name__ == '__main__':
     app.run(debug=True)
